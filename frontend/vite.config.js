7bf6604c11888dbaf9433cdd20009eb1c8fde477
ShyLuns <drpg1008@gmail.com>
2025-06-24 16:36:28 -0500

    fix: fuerza contenido Tailwind en archivos JSX

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/', // 👈 Esto es obligatorio para Render
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -13,4 +13,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
+  css: {
+    postcss: './postcss.config.js' // 🔹 Asegura que postcss se use
+  }
 })
