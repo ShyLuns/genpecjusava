6b2abd5a0adfafcf68bf999aefc2b17a4d0450db
ShyLuns <drpg1008@gmail.com>
2025-06-25 16:29:51 -0500

    plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: '/', // ✅ necesario para Render
-  plugins: [react()],
+  base: '/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.', // 👈 Esto lo pone directamente en /dist
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -12,6 +23,5 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    emptyOutDir: true
-  }
+  },
 });
